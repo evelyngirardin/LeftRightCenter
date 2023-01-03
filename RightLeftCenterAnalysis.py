aeab163dc76e36eed5d671505bb2a69f0cc7ceea
Jacob Girardin <jacobdgirardin@gmail.com>
2023-01-03 15:59:33 -0500

    Added in visualization via matplotlib

--- RightLeftCenterAnalysis.py
+++ RightLeftCenterAnalysis.py
@@ -1,7 +1,8 @@
 from Die import Die
 from Game import Game
+import matplotlib.pyplot as plt 
 
-def run_trials(number_of_players, number_of_trials=5000):
+def run_trials(number_of_players, number_of_trials=5001):
     basic_die = Die(1,1,1,3)
     basic_game = Game(number_of_players, basic_die, 3)
     winners = {}
@@ -16,7 +17,7 @@ def run_trials(number_of_players, number_of_trials=5000):
             winners[winner_name] = 1
         basic_game = Game(number_of_players, basic_die, 3)
         if n%1000==0:
-            print(n)
+            print(str(int(n/number_of_trials*100)) + "%")
 
     x_axis = []
     y_axis = []
@@ -27,10 +28,38 @@ def run_trials(number_of_players, number_of_trials=5000):
         x_axis.append(i+1)
         percentages.append(winners[str(i)]/number_of_trials)
 
-    return names, number_of_wins, percentage_of_wins
+    return x_axis, y_axis, percentages
+
+def plots(names, number_of_wins, percentage_of_wins, number_of_players):
+
+    fig, ax = plt.subplots()
+
+    ax.bar(names, percentage_of_wins, width=1, edgecolor="white", linewidth=0.7)
+
+    plt.title("Percentage of wins")
+
+    fig1, ax1 = plt.subplots()
+
+    difference_from_average = percentage_of_wins.copy()
+
+    for item in range(0, len(difference_from_average)):
+        difference_from_average[item] -= 1/number_of_players
+
+    ax1.bar(names, difference_from_average, width=1, edgecolor='white', linewidth=0.7)
+
+    plt.title("Difference between expected and actual percentage of wins")
+
+    plt.show()
+
 
 def main():
-    run_trials(10)
+    # Set up
+    number_of_players = 8
+    number_of_trials = 50000
+
+    names, number_of_wins, percentage_of_wins = run_trials(number_of_players, number_of_trials)
+    plots(names, number_of_wins, percentage_of_wins, number_of_players)
+
 
 if __name__ == '__main__':
     main()
